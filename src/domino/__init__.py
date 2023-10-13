d130c2072e7d4a21f5d858d4dba009d80a74e2bc
Vinicius Vaz <vinicvaz95@gmail.com>
2023-10-13 11:17:06 -0300

    version path update

--- src/domino/__init__.py
+++ src/domino/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def _get_version():
-    version_file_path = Path(__file__).parent / 'VERSION'
+    version_file_path = Path(__file__).parent / 'src/domino/VERSION'
 
     with version_file_path.open('r') as version_file:
         return version_file.read().strip()
